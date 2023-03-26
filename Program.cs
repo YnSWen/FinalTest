46b66bab753bfc2595a6cea2933181f9ce892608
Yana <nyyhss.08@gmail.com>
2023-03-26 17:22:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array = new string[7] { "Germany", "50", ":-(", "-76", "kitten", "7362", "0" };
+
+string[] ResultArray = GetResultArray(array);
+
+
+string[] GetResultArray(string[] array)
+{
+    int CountElements = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            CountElements++;
+        }
+    }
+
+    string[] ResultArray = new string[CountElements];
+
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            ResultArray[n] = array[i];
+            n++;
+        }
+    }
+    return ResultArray;
+}

ce1c112dbe05b18028d75ae03478a33569e78d4c
Yana <nyyhss.08@gmail.com>
2023-03-26 17:28:05 +0300

    updated code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿string[] array = new string[7] { "Germany", "50", ":-(", "-76", "kitten", "7362", "0" };
 
 string[] ResultArray = GetResultArray(array);
-
+Console.WriteLine($"[{String.Join(", ", ResultArray)}]");
 
 string[] GetResultArray(string[] array)
 {
